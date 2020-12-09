e1015c4f17f063542e40f265b28a5167ab6dfdfb
Jason Duffy <jasonsduffy@gmail.com>
2020-12-09 15:23:23 -0500

    Day 9, Part 2

--- src/day9.rs
+++ src/day9.rs
@@ -1,3 +1,5 @@
+use std::ops::Range;
+
 pub fn solve1(lines: &Vec<&str>) -> usize {
     let nums = lines.iter().flat_map(|&s| s.parse::<usize>()).collect();
 
@@ -5,7 +7,9 @@ pub fn solve1(lines: &Vec<&str>) -> usize {
 }
 
 pub fn solve2(lines: &Vec<&str>) -> usize {
-    0
+    let nums = lines.iter().flat_map(|&s| s.parse::<usize>()).collect();
+
+    crack(&nums, 25)
 }
 
 fn solve(nums: &Vec<usize>, preamble_size: usize) -> usize {
@@ -33,6 +37,35 @@ fn sum_of_two_in(n: usize, nums: &[usize]) -> bool {
     false
 }
 
+fn crack(nums: &Vec<usize>, preamble_size: usize) -> usize {
+    let target = solve(nums, preamble_size);
+
+    for i in 0..nums.len() - 1 {
+        if let Ok((min, max)) = get_sum_range_to(nums, i, target) {
+            return min + max;
+        }
+    }
+
+    0
+}
+
+fn get_sum_range_to(
+    nums: &Vec<usize>,
+    begin_index: usize,
+    target: usize,
+) -> Result<(usize, usize), ()> {
+    for i in begin_index..nums.len() - 1 {
+        let range = &nums[begin_index..i];
+        if range.iter().sum::<usize>() == target {
+            let min = range.iter().min().unwrap();
+            let max = range.iter().max().unwrap();
+            return Ok((*min, *max));
+        }
+    }
+
+    Err(())
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -45,5 +78,15 @@ mod tests {
         ];
 
         assert_eq!(solve(&nums, 5), 127);
+    }
+
+    #[test]
+    fn example2() {
+        let nums = vec![
+            35, 20, 15, 25, 47, 40, 62, 55, 65, 95, 102, 117, 150, 182, 127, 219, 299, 277, 309,
+            576,
+        ];
+
+        assert_eq!(crack(&nums, 5), 62);
     }
 }
