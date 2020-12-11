7d1f516e67489d09b07dfb18e94bd13af77f40c1
Jason Duffy <jasonsduffy@gmail.com>
2020-12-11 11:35:58 -0500

    Day11, Part2

--- src/day11.rs
+++ src/day11.rs
@@ -1,24 +1,28 @@
 pub fn solve1(lines: &Vec<&str>) -> usize {
+    solve(lines, &iterate_adjacent)
+}
+
+pub fn solve2(lines: &Vec<&str>) -> usize {
+    solve(lines, &iterate_vision)
+}
+
+fn solve(lines: &Vec<&str>, iteration: &dyn Fn(&Vec<Vec<char>>) -> Vec<Vec<char>>) -> usize {
     let mut matrix = lines
         .iter()
         .map(|line| line.chars().collect::<Vec<_>>())
         .collect::<Vec<_>>();
 
     let mut last_occupied = get_occupied(&matrix);
-    matrix = iterate(&matrix);
+    matrix = iteration(&matrix);
     let mut occupied = get_occupied(&matrix);
 
     while occupied != last_occupied {
-        matrix = iterate(&matrix);
+        matrix = iteration(&matrix);
         last_occupied = occupied;
         occupied = get_occupied(&matrix);
     }
 
     occupied
-}
-
-pub fn solve2(lines: &Vec<&str>) -> usize {
-    0
 }
 
 fn get_occupied(matrix: &Vec<Vec<char>>) -> usize {
@@ -35,7 +39,7 @@ fn occupied(c: &char) -> usize {
     }
 }
 
-fn iterate(matrix: &Vec<Vec<char>>) -> Vec<Vec<char>> {
+fn iterate_adjacent(matrix: &Vec<Vec<char>>) -> Vec<Vec<char>> {
     matrix
         .iter()
         .enumerate()
@@ -56,6 +60,29 @@ fn iterate(matrix: &Vec<Vec<char>>) -> Vec<Vec<char>> {
                 .collect()
         })
         .collect()
+}
+
+fn iterate_vision(matrix: &Vec<Vec<char>>) -> Vec<Vec<char>> {
+    matrix
+        .iter()
+        .enumerate()
+        .map(|(j, y)| {
+            y.iter()
+                .enumerate()
+                .map(|(i, x)| match x {
+                    'L' => match vision_occupied(matrix, j, i) {
+                        0 => '#',
+                        _ => 'L',
+                    },
+                    '#' => match vision_occupied(matrix, j, i) {
+                        n if n >= 5 => 'L',
+                        _ => '#',
+                    },
+                    &state => state,
+                })
+                .collect()
+        })
+        .collect::<Vec<Vec<char>>>()
 }
 
 fn adjacent_occupied(matrix: &Vec<Vec<char>>, row: usize, col: usize) -> usize {
@@ -89,13 +116,128 @@ fn adjacent_occupied(matrix: &Vec<Vec<char>>, row: usize, col: usize) -> usize {
     result
 }
 
+fn vision_occupied(matrix: &Vec<Vec<char>>, row: usize, col: usize) -> usize {
+    let mut result = 0;
+    result += nw_occupied(matrix, row, col);
+    result += n_occupied(matrix, row, col);
+    result += ne_occupied(matrix, row, col);
+    result += e_occupied(matrix, row, col);
+    result += se_occupied(matrix, row, col);
+    result += s_occupied(matrix, row, col);
+    result += sw_occupied(matrix, row, col);
+    result += w_occupied(matrix, row, col);
+    result
+}
+
+fn nw_occupied(matrix: &Vec<Vec<char>>, row: usize, col: usize) -> usize {
+    if row <= 0 || col <= 0 {
+        0
+    } else {
+        match matrix[row - 1][col - 1] {
+            '#' => 1,
+            'L' => 0,
+            '.' => nw_occupied(matrix, row - 1, col - 1),
+            _ => panic!("invalid char"),
+        }
+    }
+}
+
+fn n_occupied(matrix: &Vec<Vec<char>>, row: usize, col: usize) -> usize {
+    if row <= 0 {
+        0
+    } else {
+        match matrix[row - 1][col] {
+            '#' => 1,
+            'L' => 0,
+            '.' => n_occupied(matrix, row - 1, col),
+            _ => panic!("invalid char"),
+        }
+    }
+}
+
+fn ne_occupied(matrix: &Vec<Vec<char>>, row: usize, col: usize) -> usize {
+    if row <= 0 || col >= matrix[0].len() - 1 {
+        0
+    } else {
+        match matrix[row - 1][col + 1] {
+            '#' => 1,
+            'L' => 0,
+            '.' => ne_occupied(matrix, row - 1, col + 1),
+            _ => panic!("invalid char"),
+        }
+    }
+}
+
+fn e_occupied(matrix: &Vec<Vec<char>>, row: usize, col: usize) -> usize {
+    if col >= matrix[0].len() - 1 {
+        0
+    } else {
+        match matrix[row][col + 1] {
+            '#' => 1,
+            'L' => 0,
+            '.' => e_occupied(matrix, row, col + 1),
+            _ => panic!("invalid char"),
+        }
+    }
+}
+
+fn se_occupied(matrix: &Vec<Vec<char>>, row: usize, col: usize) -> usize {
+    if row >= matrix.len() - 1 || col >= matrix[0].len() - 1 {
+        0
+    } else {
+        match matrix[row + 1][col + 1] {
+            '#' => 1,
+            'L' => 0,
+            '.' => se_occupied(matrix, row + 1, col + 1),
+            _ => panic!("invalid char"),
+        }
+    }
+}
+
+fn s_occupied(matrix: &Vec<Vec<char>>, row: usize, col: usize) -> usize {
+    if row >= matrix.len() - 1 {
+        0
+    } else {
+        match matrix[row + 1][col] {
+            '#' => 1,
+            'L' => 0,
+            '.' => s_occupied(matrix, row + 1, col),
+            _ => panic!("invalid char"),
+        }
+    }
+}
+
+fn sw_occupied(matrix: &Vec<Vec<char>>, row: usize, col: usize) -> usize {
+    if row >= matrix.len() - 1 || col <= 0 {
+        0
+    } else {
+        match matrix[row + 1][col - 1] {
+            '#' => 1,
+            'L' => 0,
+            '.' => sw_occupied(matrix, row + 1, col - 1),
+            _ => panic!("invalid char"),
+        }
+    }
+}
+
+fn w_occupied(matrix: &Vec<Vec<char>>, row: usize, col: usize) -> usize {
+    if col <= 0 {
+        0
+    } else {
+        match matrix[row][col - 1] {
+            '#' => 1,
+            'L' => 0,
+            '.' => w_occupied(matrix, row, col - 1),
+            _ => panic!("invalid char"),
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn example1() {
-        let seats = "L.LL.LL.LL
+    const STARTING_LAYOUT: &str = "L.LL.LL.LL
 LLLLLLL.LL
 L.L.L..L..
 LLLL.LL.LL
@@ -104,10 +246,19 @@ L.LLLLL.LL
 ..L.L.....
 LLLLLLLLLL
 L.LLLLLL.L
-L.LLLLL.LL"
-            .lines()
-            .collect::<Vec<&str>>();
+L.LLLLL.LL";
+
+    #[test]
+    fn example1() {
+        let seats = STARTING_LAYOUT.lines().collect::<Vec<&str>>();
 
         assert_eq!(solve1(&seats), 37);
+    }
+
+    #[test]
+    fn example2() {
+        let seats = STARTING_LAYOUT.lines().collect::<Vec<&str>>();
+
+        assert_eq!(solve2(&seats), 26);
     }
 }
